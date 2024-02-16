a9224af92841f3df2f18a8e6931d0d227f01844d
Daniel Braaksma <danebraaksma@hotmail.com>
2024-02-15 19:21:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { sayHi, sayBye } from "./utils.js";
+
+sayHi();
+
+sayBye();
+
+// const { sayHi } = require("./utils");
+
+// sayHi();
+
+
+// async function fetchData() {
+//     try {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//       const data = await response.json();
+//       console.log(data);
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   }
+
+// fetchData();
---
+++ utils.js
@@ -0,0 +1,15 @@
+function sayHi() {
+  console.log("hi");
+}
+
+function sayBye(){
+    console.log("bye");
+}
+
+export { sayHi, sayBye };
+
+// function sayHi() {
+//   console.log("hi");
+// }
+
+// module.exports = { sayHi }
